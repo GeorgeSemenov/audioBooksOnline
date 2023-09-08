62366c1d724d491e723d01a73f9d471b90a91a07
GeorgeSemenov <zhora.sem@yandex.ru>
2023-09-08 06:10:17 +0300

    kek puk

---
+++ abo/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+// require  book
+
+function App() {
+  return (
+    <div className="App">
+      <figure>
+        <figcaption>Listen to blade itself mothefaka</figcaption>
+        <audio controls src="/media/cc0-audio/t-rex-roar.mp3">
+          <a href="/media/cc0-audio/t-rex-roar.mp3"> Download audio </a>
+        </audio>
+      </figure>
+    </div>
+  );
+}
+
+export default App;
