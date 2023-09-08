145ea98d25eef472ca5c8604a18de14c0e381111
Семенов <@RIB.local>
2023-09-08 11:52:20 +0300

    some refactor added

--- abots/src/App.tsx
+++ abots/src/App.tsx
@@ -1,8 +1,7 @@
-import React, { useRef, useState } from "react";
+import React from "react";
 import "./App.css";
 
 function App() {
-  const myRef = useRef<HTMLAudioElement>(null);
   let aduioBegining: number,
     audioDuration: number = 3000;
   const audio = document.getElementById("audio") as HTMLAudioElement;
