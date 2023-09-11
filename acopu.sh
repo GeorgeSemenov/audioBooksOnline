d84e5caf0039782d820426f4f39527419ea9f49e
GeorgeSemenov <zhora.sem@yandex.ru>
2023-09-11 06:14:35 +0300

    add hosting

--- acopu.sh
+++ acopu.sh
@@ -6,3 +6,7 @@ git commit -m "${messagues}"
 git push origin
 #Для его работоспособности, пришлось перевести репозиторий с https на ssh
 #Чтобы при push он не запрашивал логин и пароль
+
+хвост : vh358.timeweb.ru
+порнзователь : ch54977
+Пуарыло: O3pOjTLJHxtknHEVHEVHE345WO0Ozw
