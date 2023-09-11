0c2d0230dce50492ff2c0a2a37a4c2874f3dbf11
Семенов <@RIB.local>
2023-09-11 10:33:39 +0300

    -m i don't know

--- abo/src/App.js
+++ abo/src/App.js
@@ -1,17 +1,76 @@
+import React, { useEffect, useRef } from "react";
+import AudioPlayer from "react-h5-audio-player";
+import "react-h5-audio-player/lib/styles.css";
 import "./App.css";
-// require  book
+const book = require("./audio/book.mp3");
 
-function App() {
+export default function App() {
+  useEffect(() => {
+    document.title = "(eng)Audio books онлине";
+  });
+  let listeningTimeBegining,
+    listeningTimeDuration = 10; //time in seconds
+  const audioPlayer = useRef(null);
+  const aud = audioPlayer?.current?.audio.current;
+  const yearInSeconds = 60 * 60 * 24 * 30 * 12;
   return (
-    <div className="App">
-      <figure>
-        <figcaption>Listen to blade itself mothefaka</figcaption>
-        <audio controls src="/media/cc0-audio/t-rex-roar.mp3">
-          <a href="/media/cc0-audio/t-rex-roar.mp3"> Download audio </a>
-        </audio>
-      </figure>
-    </div>
+    <AudioPlayer
+      src={setAudioBegining(book, getAudioBegining())}
+      // src={`https://sds.hugesound.ru/sounds/hugesoundru__kids-4.mp3#t=${getAudioBegining()}`}
+      onPlay={() => {
+        console.log(`onPlay`);
+        listeningTimeBegining = aud ? Math.trunc(aud.currentTime) : 0;
+      }}
+      onListen={() => {
+        console.log(`aud`, aud);
+        console.log(`onListen`);
+        if (aud) {
+          if (listeningTimeBegining + listeningTimeDuration < aud.currentTime) {
+            aud.pause();
+          }
+        }
+      }}
+      onPause={() => {
+        console.log(`onPause`);
+        if (aud)
+          document.cookie = `listenedTime = ${Math.trunc(
+            aud.currentTime
+          )}; max-age=${yearInSeconds}`;
+      }}
+      ref={audioPlayer}
+    />
   );
 }
 
-export default App;
+function setAudioBegining(audioUrl, beginFrom) {
+  console.log(`setAudioBegining`);
+  const resultetdUrl = `${audioUrl}#t=${beginFrom}`;
+  return resultetdUrl;
+}
+
+function getStringForAudio(seconds) {
+  console.log(`getStringForAudio`);
+  const pad = (x) => {
+    return x < 10 ? `0${x}` : `${x}`;
+  };
+  return `${pad(Math.trunc(seconds / (60 * 60)))}:${pad(
+    Math.trunc((seconds / 60) % 60)
+  )}:${pad(seconds % 60)}`;
+}
+
+function getAudioBegining() {
+  console.log(`getAudioBegining`);
+  return getCookie("listenedTime")
+    ? getStringForAudio(getCookie("listenedTime"))
+    : "00:00:00";
+}
+
+function getCookie(cookieName) {
+  console.log(`getCookie`);
+  const cookieObj = document.cookie.split("; ").reduce((prev, current) => {
+    const [name, ...value] = current.split("=");
+    prev[name] = value.join("=");
+    return prev;
+  }, {});
+  return cookieObj[cookieName];
+}
